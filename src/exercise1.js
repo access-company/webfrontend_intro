1e521dac34e918ad7b8d44e6a18e46d49f579425
Syori.Kano <Syori.Kano@Y55-66743.local>
2019-05-22 14:22:42 +0900

    exercise1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,17 +1,17 @@
 var filter = function(items, iteratee, context) {
-  var thisArg = context ? context : this;
-  var values = [];
-  var i, l;
-  for (i = 0, l = items.length; i < l; i++) {
+  const thisArg = context ? context : this;
+  const l = items.length
+  let values = [];
+  for (let i = 0; i < items.length; i++) {
     if (iteratee.call(thisArg, items[i], i, items)) {
       values.push(items[i]);
     }
   }
   return values;
 };
-var a = [1, 2, 3];
-var filtered = filter(a, function(v) {
+
+const a = [1, 2, 3];
+const filtered = filter(a, function(v) {
   return v === 2;
 });
-var a = filtered[0];
-console.log(a);
+console.log(filtered[0]);
