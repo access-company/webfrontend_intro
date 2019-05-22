e5e3d6e5c03bc5510fa26b5b271b50799d8741c0
hiroshi.nishigami@access-company.com <Hiroshi.Nishigami@Y55-66745.local>
2019-05-22 15:34:43 +0900

    modified:   exercise8.mjs

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.github.com/users/github')
+fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myJson   => console.log(myJson.followers_url ));
