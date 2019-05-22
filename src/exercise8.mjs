cc3b3987bf7b6f4ac761672e9da8b458a672b750
irisTa56 <takayuki.kobayashi@access-company.com>
2019-05-22 15:32:29 +0900

    refine exercise8.mjs

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,6 +1,12 @@
 import fetch from 'node-fetch';
 
+const fetchFollowers = (url) =>  {
+  fetch(url)
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
 fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
-  .then((res) => res.json())
-  .then((json) => json.followers_url)
-  .then((data) => console.log(data));
+  .then(res => res.json())
+  .then(json => json.followers_url)
+  .then(url => fetchFollowers(url));
