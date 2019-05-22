3e31b2f9b2f38e90934977809f14e3dbdda8ebbc
KanameTakano <kaname.takano@access-company.com>
2019-05-22 14:29:58 +0900

    fin exercise2.js

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,10 +1,11 @@
 const newUser = function(id, firstName, lastName, age) {
   const fullName = firstName + " " + lastName;
   return {
-    id: id,
+    id,
     name: fullName,
-    age: age,
+    age,
   };
 };
 const taro = newUser("51ff0475d615329700235136", "Taro", "Yamada", 14);
+
 console.log(taro);
