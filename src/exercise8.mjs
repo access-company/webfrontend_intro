1fd74146d3db28689e05cdebb7424912165b1459
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-22 15:38:46 +0900

    exercise 7..8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.github.com/users/github')
+fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myJson   => fetch(myJson.followers_url))
+  .then(responce => responce.json())
+  .then(json => console.log(json));
