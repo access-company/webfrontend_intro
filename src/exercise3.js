cb0be6389f938f6eb702432f90f70ba19cfe95dc
Masamitsu.Yakushiji <Masamitsu.Yakushiji@Y55-66727.local>
2019-05-22 15:22:18 +0900

    ex3-3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,8 +1,8 @@
 const inspectUser = (user) => {
   const{id, session:{key , expiresAt}}=user.data
-  console.log("id = " + id);
-  console.log("key = " + key);
-  console.log("expiresAt = " + expiresAt);
+  console.log(`id = ${id}`);
+  console.log(`key = ${key}`);
+  console.log(`expiresAt = ${expiresAt}`);
 };
 
 inspectUser({
