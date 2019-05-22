d56489dbbf37691ab7c1455d0986ec5e1a08d995
aLeSangK <le.sang@access-company.com>
2019-05-22 14:37:31 +0900

    first by raku

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,7 @@
 var filter = function(items, iteratee, context) {
-  var thisArg = context ? context : this;
-  var values = [];
-  var i, l;
+  const thisArg = context ? context : this;
+  const values = [];
+  let i, l;
   for (i = 0, l = items.length; i < l; i++) {
     if (iteratee.call(thisArg, items[i], i, items)) {
       values.push(items[i]);
@@ -9,9 +9,9 @@ var filter = function(items, iteratee, context) {
   }
   return values;
 };
-var a = [1, 2, 3];
-var filtered = filter(a, function(v) {
+let a = [1, 2, 3];
+let filtered = filter(a, function(v) {
   return v === 2;
 });
-var a = filtered[0];
+ a = filtered[0];
 console.log(a);
