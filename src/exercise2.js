ecc5b4361ccf8299132e0e5b829334f754557ee9
longniu <niu.long@access-company.com>
2019-05-22 15:59:31 +0900

    update 課題２

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,10 +1,12 @@
 const newUser = function(id, firstName, lastName, age) {
-  const fullName = firstName + " " + lastName;
-  return {
-    id: id,
-    name: fullName,
-    age: age,
-  };
+  const name = firstName + " " + lastName;
+  // return {
+  //   id: id,
+  //   name: fullName,
+  //   age: age,
+  // };
+  const user = {id, name, age};
+  return user;
 };
 const taro = newUser("51ff0475d615329700235136", "Taro", "Yamada", 14);
 console.log(taro);
