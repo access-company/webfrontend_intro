90f577b7056a29e1517ed77706721a66d05c1546
irisTa56 <takayuki.kobayashi@access-company.com>
2019-05-22 14:13:15 +0900

    change exercise1.js

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,17 +1,3 @@
-var filter = function(items, iteratee, context) {
-  var thisArg = context ? context : this;
-  var values = [];
-  var i, l;
-  for (i = 0, l = items.length; i < l; i++) {
-    if (iteratee.call(thisArg, items[i], i, items)) {
-      values.push(items[i]);
-    }
-  }
-  return values;
-};
-var a = [1, 2, 3];
-var filtered = filter(a, function(v) {
-  return v === 2;
-});
-var a = filtered[0];
-console.log(a);
+const a = [1, 2, 3];
+const filtered = a.filter((v) => v == 2);
+console.log(filtered[0]);
