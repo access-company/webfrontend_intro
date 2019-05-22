d611a907810ab92edbcf250b5f7c19fd66572fc3
renya.kinugawa <renya.kinugawa@access-company.com>
2019-05-22 14:52:39 +0900

    exercise2.js

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,17 +1,11 @@
 const filter = function (items, iteratee, context) {
   const thisArg = context ? context : this;
   const values = [];
-  // const l = items.length
   for (let i = 0, l = items.length; i < l; i++) {
     if (iteratee.call(thisArg, items[i], i, items)) {
       values.push(items[i]);
     }
   }
-  // for (const item in items) {
-  //   if (iteratee.call(thisArg, item)) {
-  //     values.push(item);
-  //   }
-  // }
   return values;
 };
 let a = [1, 2, 3];
