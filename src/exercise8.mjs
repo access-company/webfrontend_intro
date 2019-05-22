66164d61e047793da54a54f5ef10bc04e279ae85
irisTa56 <takayuki.kobayashi@access-company.com>
2019-05-22 16:04:38 +0900

    exercise8.mjs without callback

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,12 +1,8 @@
 import fetch from 'node-fetch';
 
-const fetchFollowers = (url) =>  {
-  fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json));
-}
-
 fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
   .then(res => res.json())
   .then(json => json.followers_url)
-  .then(url => fetchFollowers(url));
+  .then(url => fetch(url))
+  .then(res => res.json())
+  .then(json => console.log(json));
