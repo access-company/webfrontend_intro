845d5a88c842bff1b1072e346c5d1f20a1a0300e
aYukiYoshida <yuki.yoshida@access-company.com>
2019-05-22 14:46:26 +0900

    Change exercise3.js for 3-2

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,4 +1,4 @@
-const inspectUser = function(user) {
+const inspectUser = user => {
   const { data: {id, session: {key, expiresAt}}} = user
   console.log("id = " + id);
   console.log("key = " + key);
