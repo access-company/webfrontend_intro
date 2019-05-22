e35e3ec8bed22b117f3466b015083b28ef26989e
irisTa56 <takayuki.kobayashi@access-company.com>
2019-05-22 15:13:45 +0900

    change exercise7.js

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,16 @@
+const consoleLog = (msg) => {
+  process.stdout.write(msg);
+}
+
 setTimeout(() => {
-  console.log('Hello');
+  consoleLog('Hello');
+  setTimeout(() => {
+    consoleLog(' World');
+    setTimeout(() => {
+      consoleLog('!');
+      setTimeout(() => {
+        consoleLog('!\n');
+      }, 1000);
+    }, 2000);
+  }, 3000);
 }, 1000);
