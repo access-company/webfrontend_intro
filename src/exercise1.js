4bc66bb4efceb76006f32e45163e812f8330a873
AtsumoriTakahashi <atakahashi.43nc542@gmail.com>
2019-05-22 14:25:14 +0900

    [fix] problem 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,17 +1,16 @@
 var filter = function(items, iteratee, context) {
-  var thisArg = context ? context : this;
-  var values = [];
-  var i, l;
-  for (i = 0, l = items.length; i < l; i++) {
+  const thisArg = context ? context : this;
+  let values = [];
+  for (let i = 0, l = items.length; i < l; i++) {
     if (iteratee.call(thisArg, items[i], i, items)) {
       values.push(items[i]);
     }
   }
   return values;
 };
-var a = [1, 2, 3];
-var filtered = filter(a, function(v) {
+let a = [1, 2, 3];
+const filtered = filter(a, function(v) {
   return v === 2;
 });
-var a = filtered[0];
+a = filtered[0];
 console.log(a);
