6c74a19bf311bd1653df6c4f1a2379a5589af838
hiroshi.nishigami@access-company.com <Hiroshi.Nishigami@Y55-66745.local>
2019-05-22 14:41:54 +0900

    modified:   exercise3.js

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,9 +1,8 @@
 const inspectUser = user => {
   const {data: {id, session: {key, expiresAt}}} = user
-  console.log("id = " + id);
-  console.log("key = " + key);
-  console.log("expiresAt = " + expiresAt);
-};
+  console.log(`id = ${id}
+key = ${key}
+expiresAt = ${expiresAt}`)};
 
 inspectUser({
   data: {
