c87ae746fd8b6caeeb2d0352f12d0f643f390e9f
pnt <ngocthach.pham@access-company.com>
2019-05-22 14:42:04 +0900

    exrcise8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -2,4 +2,7 @@ import fetch from 'node-fetch';
 
 fetch('https://api.github.com/users/github')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myJson   => fetch(myJson.followers_url)
+  .then(response => response.json())
+  .then(followers_url => console.log(followers_url))
+);
