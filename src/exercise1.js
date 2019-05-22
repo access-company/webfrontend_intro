79321a2256fe303c94b55fbe37078c4c72d3d919
aKokiIto <koki.ito@access-company.com>
2019-05-22 14:37:21 +0900

    fix exercise1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,17 +1,6 @@
-var filter = function(items, iteratee, context) {
-  var thisArg = context ? context : this;
-  var values = [];
-  var i, l;
-  for (i = 0, l = items.length; i < l; i++) {
-    if (iteratee.call(thisArg, items[i], i, items)) {
-      values.push(items[i]);
-    }
-  }
-  return values;
-};
-var a = [1, 2, 3];
-var filtered = filter(a, function(v) {
-  return v === 2;
-});
-var a = filtered[0];
-console.log(a);
+const a = [1, 2, 3, 2];
+const filtered = ((items, iteratee, context) => {
+  const thisArg = context ? context : this;
+  return items.filter((item, i, array) => iteratee.call(thisArg, item, i, array));
+})(a, v => v === 2);
+console.log(filtered[0]);
