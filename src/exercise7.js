cda3703f4d54c3b7e5dba2f685ee0d4f5ec2b7e3
longniu <niu.long@access-company.com>
2019-05-23 14:07:07 +0900

    課題７

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,26 @@
+const printOut = (aisatu, waitTime) => {
+  console.log(`${aisatu} (${waitTime}秒後)`);}
+
+// printOut("Hello",1);
+// setTimeout(printOut('Hello',1),1000);
+// setTimeout(printOut('World',1),3000);
+// setTimeout(printOut('!',1),1000);
+// setTimeout(printOut('!',1),1000);
+
+// setTimeout(() => {
+//   console.log('Hello world!!'); // 1000[ms] 後に 'Hello world!!' が表示される
+// }, 1000);
+
 setTimeout(() => {
-  console.log('Hello');
+  printOut("Hello",1);
+  setTimeout(()=>{
+    printOut("World",3);
+    setTimeout(()=>{
+      printOut("!",2);
+      setTimeout(()=>{
+        printOut("!",1);
+      },1000);
+    },2000);
+  },3000);
 }, 1000);
+// console.log("t");
