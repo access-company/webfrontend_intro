a4e4ac026cefe2dd3baf724e69759f2ea668faf0
aKokiIto <koki.ito@access-company.com>
2019-05-23 10:52:49 +0900

    fix exercise3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,13 +1,10 @@
-const inspectUser = (user) => {
-  const data = user.data;
-  const session = data.session;
-  console.log(`id = ${data.id}`);
-  console.log(`key = ${session.key}`);
-  console.log(`expiresAt = ${session.expiresAt}`);
+const inspectUser = ( {data: {id, session: {key, expiresAt}}} ) => {
+  console.log(`id = ${id}`);
+  console.log(`key = ${key}`);
+  console.log(`expiresAt = ${expiresAt}`);
 };
 
-inspectUser( {data: {id, session: {key, expiresAt}}}
-  = {data: {
+inspectUser( {data: {
     id: "51ff0475d615329700235136",
     session: {
       key: "NoBtELh82txWnMb5kEQJ",
