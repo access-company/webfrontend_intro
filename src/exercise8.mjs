fef056c75e77d77fcff67c357c6fcc8c37e0b7c1
aYukiYoshida <yuki.yoshida@access-company.com>
2019-05-23 10:43:50 +0900

    Change exercise8.mjs for Ex.8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.github.com/users/github')
-  .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+fetch('https://api.github.com/users/diescake')
+  .then(res1 => res1.json())
+  .then(myJson => fetch(myJson.followers_url))
+  .then(res2 => res2.json())
+  .then(flJson => console.log(flJson));
