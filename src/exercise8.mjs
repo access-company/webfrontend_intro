fe0bcf8312c605756061563a1844350b91c4a895
aKokiIto <koki.ito@access-company.com>
2019-05-23 09:52:57 +0900

    update exercise8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -2,4 +2,8 @@ import fetch from 'node-fetch';
 
 fetch('https://api.github.com/users/github')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myJson   => myJson.followers_url)
+  .then(url => fetch(url)
+    .then(response => response.json()
+    .then(json => console.log(json)))
+  )
