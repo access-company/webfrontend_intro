43a9e3614ecd5fe5ade5eeed9fb914e072e5bf1b
Syori.Kano <Syori.Kano@Y55-66743.local>
2019-05-23 10:20:30 +0900

    exercise9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
-  const usersResponse = await fetch(url);
-  const users         = await usersResponse.json();
-  console.log(users);
+  const url = 'http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json';
+  const res = await fetch(url);
+  const json = await res.json();
+  const res2 = await fetch(json.followers_url);
+  console.log(await res2.json());
 }
 
 fetchFollowers();
