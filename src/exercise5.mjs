dff7bb43744b7a2bd58673263a83ef5608ff2e5b
longniu <niu.long@access-company.com>
2019-05-23 11:32:35 +0900

    課題５

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -5,12 +5,28 @@ class User {
     this.id = id;
   }
   // prototypeメソッド getName()を実装する
+  getName(){
+    return this.name;
+  };
+
   // prototypeメソッド getAge()を実装する
+  getAge(){
+    return this.age;
+  };
+
   // staticメソッド equal(user1, user2)を実装する
+  static equal(user1, user2){
+    if(user1.name == user2.name && user1.age == user2.age && user1.id == user2.id){
+      return true;
+    }else{
+      return false;
+    }
+  };
 }
+
 const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
 console.log(user1.getName());
 console.log(user1.getAge());
 
 const user2 = new User('74ff0520d615329720235156', 'Hanako', 14);
-console.log(`${user1.name} is ${user2.name}: ${User.equal(user1, user2)}`);
+console.log(`${user1.name} is ${user2.name}: ${User.equal(user2, user1)}`);
