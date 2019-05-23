35494159c47acb37eed0359e94dee6b5306add67
longniu <niu.long@access-company.com>
2019-05-23 11:42:21 +0900

    課題６

---
+++ src/exercise6.mjs
@@ -0,0 +1,8 @@
+import User from './exercise5'
+
+const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
+console.log(user1.getName());
+console.log(user1.getAge());
+
+const user2 = new User('74ff0520d615329720235156', 'Hanako', 14);
+console.log(`${user1.name} is ${user2.name}: ${User.equal(user2, user1)}`);
