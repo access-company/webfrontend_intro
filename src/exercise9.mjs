70aa52446b4341e9f5f12e3d9ea5aab50e7ee1e8
aYukiYoshida <yuki.yoshida@access-company.com>
2019-05-23 13:41:57 +0900

    Change exercise9.mjs for Ex.9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
-  const usersResponse = await fetch(url);
-  const users         = await usersResponse.json();
-  console.log(users);
+  const url               = 'https://api.github.com/users/diescake';
+  const userResponse      = await fetch(url);
+  const userJson          = await userResponse.json();
+  const followersResponse = await fetch(userJson.followers_url);
+  const followersJson     = await followersResponse.json();
+  console.log(followersJson);
 }
 
 fetchFollowers();
