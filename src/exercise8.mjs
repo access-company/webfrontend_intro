6ed53502cfaba3a0b95b40170922619c6611e900
haruka akaba <haruka.akaba@access-company.com>
2019-05-23 16:07:47 +0900

    finish Ex.8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,9 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.github.com/users/github')
+fetch('https://api.github.com/users/diescake')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myJson   => myJson.followers_url)
+  .then(followers_url => fetch(followers_url)
+    .then(response => response.json())
+    .then(myJson   => console.log(myJson))
+    );
