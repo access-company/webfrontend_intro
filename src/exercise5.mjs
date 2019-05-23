a222d06d6d4920528b76d704f6b30c39d97e8d22
Masamitsu.Yakushiji <Masamitsu.Yakushiji@Y55-66727.local>
2019-05-23 11:42:35 +0900

    ex5

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -5,8 +5,17 @@ class User {
     this.id = id;
   }
   // prototypeメソッド getName()を実装する
+  getName(){
+
+  }
   // prototypeメソッド getAge()を実装する
+  getAge(){
+
+  }
   // staticメソッド equal(user1, user2)を実装する
+  static equal(user1,user2){
+    
+  }
 }
 const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
 console.log(user1.getName());
