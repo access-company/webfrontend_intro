6c2e8e926e5d1c0929873e3533ddf5334560a883
diescake <daisuke.kondo@access-company.com>
2019-05-23 11:23:57 +0900

    wip

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,17 +1,20 @@
-const inspectUser = function(user) {
-  const data = user.data;
-  const session = data.session;
-  console.log("id = " + data.id);
-  console.log("key = " + session.key);
-  console.log("expiresAt = " + session.expiresAt);
+const inspectUser = function({
+  data: {
+    id,
+    session: { key, expiresAt }
+  }
+}) {
+  console.log('id = ' + id);
+  console.log('key = ' + key);
+  console.log('expiresAt = ' + expiresAt);
 };
 
 inspectUser({
   data: {
-    id: "51ff0475d615329700235136",
+    id: '51ff0475d615329700235136',
     session: {
-      key: "NoBtELh82txWnMb5kEQJ",
-      expiresAt: "2013-10-22T10:04:20+00:00",
-    },
-  },
+      key: 'NoBtELh82txWnMb5kEQJ',
+      expiresAt: '2013-10-22T10:04:20+00:00'
+    }
+  }
 });
