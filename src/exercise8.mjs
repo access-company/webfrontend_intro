cb4c57e5eaf858d0048f9dbc981fca1697acb7d9
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-23 11:37:44 +0900

    Updated EX.8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,7 @@
 import fetch from 'node-fetch';
 
-fetch('https://api.github.com/users/github')
+fetch('https://api.github.com/users/diescake')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(json     => fetch(json.followers_url)
+                    .then(res => res.json())
+                    .then(res => console.log(res)));
