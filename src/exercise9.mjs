908366b9e7319e47e711e86ba4d7168bef04bd0f
AtsumoriTakahashi <atakahashi.43nc542@gmail.com>
2019-05-23 11:03:38 +0900

    [fix] exercise 9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -3,8 +3,12 @@ import fetch from 'node-fetch';
 async function fetchFollowers() {
   const url           = 'https://api.github.com/users/github';
   const usersResponse = await fetch(url);
-  const users         = await usersResponse.json();
-  console.log(users);
+  const users = await usersResponse.json();
+  const followersURL = users.followers_url
+  const followersResponse = await fetch(followersURL);
+  const followers = await followersResponse.json();
+  
+  console.log(followers);
 }
 
 fetchFollowers();
