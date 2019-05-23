b717987556b47696f73da2864849786afb470476
KanameTakano <kaname.takano@access-company.com>
2019-05-23 11:04:28 +0900

    fin ex9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,14 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
+  const url           = 'https://api.github.com/users/diescake';
   const usersResponse = await fetch(url);
   const users         = await usersResponse.json();
-  console.log(users);
+
+  const followersResponse = await fetch(users.followers_url);
+  const followers = await followersResponse.json();
+  
+  console.log(JSON.stringify(followers, undefined, "🍙")); //rice ball
 }
 
 fetchFollowers();
