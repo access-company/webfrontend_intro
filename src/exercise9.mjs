90b210b68c206fabcd2e2ea471127c49ea6c160a
haruka akaba <haruka.akaba@access-company.com>
2019-05-23 16:19:31 +0900

    finish Ex.9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -4,7 +4,10 @@ async function fetchFollowers() {
   const url           = 'https://api.github.com/users/github';
   const usersResponse = await fetch(url);
   const users         = await usersResponse.json();
-  console.log(users);
+  const followers_url = await users.followers_url;
+  const response      = await fetch(followers_url);
+  const myJson        = await response.json();
+  console.log(myJson);
 }
 
 fetchFollowers();
