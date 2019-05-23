6cc20b81bea6c84946ab0828883a0f15b084206f
KanameTakano <kaname.takano@access-company.com>
2019-05-23 09:58:36 +0900

    fin ex7

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,19 @@
+// call back 地獄
 setTimeout(() => {
   console.log('Hello');
+  
+  setTimeout(() => {
+    console.log('World');
+
+    setTimeout(() => {
+      console.log('!');
+
+      setTimeout(() => {
+        console.log('!');
+      }, 1000);
+
+    }, 2000);
+
+  }, 3000);
+
 }, 1000);
