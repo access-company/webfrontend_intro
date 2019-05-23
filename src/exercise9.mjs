335787a9ea032dcbe7cdd805024a370baf34aa0d
aYukiMizuno <yuki.mizuno@access-company.com>
2019-05-23 10:15:23 +0900

    exercise 9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,12 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
-  const usersResponse = await fetch(url);
-  const users         = await usersResponse.json();
-  console.log(users);
+  const url               = 'https://api.github.com/users/diescake';
+  const usersResponse     = await fetch(url);
+  const users             = await usersResponse.json();
+  const followersResponce = await fetch(users.followers_url);
+  const followers         = await followersResponce.json();
+  console.log(followers);
 }
 
 fetchFollowers();
