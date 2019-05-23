9fab7027b0a4455ab3df92c4749736f1752cd5a9
longniu <niu.long@access-company.com>
2019-05-23 16:12:21 +0900

    課題９

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,19 @@
 import fetch from 'node-fetch';
 
+// fetch('https://api.github.com/users/diescake')
+//   .then(response => response.json())
+//   .then(myJson  => fetch(myJson.followers_url))
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
+  const url           = 'https://api.github.com/users/diescake';
   const usersResponse = await fetch(url);
   const users         = await usersResponse.json();
-  console.log(users);
+  const fol_url       = await users.followers_url;
+  const for_Response  = await fetch(fol_url);
+  const fols          = await for_Response.json();
+  console.log(fols);
 }
 
 fetchFollowers();
