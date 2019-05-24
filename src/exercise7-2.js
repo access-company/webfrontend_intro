98fd89fbd5eec01b958343eb2e7699bd7c6f8fdf
Syori.Kano <Syori.Kano@Y55-66743.local>
2019-05-24 11:50:58 +0900

    refactor exercise7-2

--- src/exercise7-2.js
+++ src/exercise7-2.js
@@ -8,6 +8,6 @@ function showMsgAsync(msg, waitspan) {
 }
 
 showMsgAsync('hello', 1000)
-    .then(p => showMsgAsync('World', 3000))
-    .then(p => showMsgAsync('!', 2000))
-    .then(p => showMsgAsync('!', 1000));  
+    .then(() => showMsgAsync('World', 3000))
+    .then(() => showMsgAsync('!', 2000))
+    .then(() => showMsgAsync('!', 1000));
