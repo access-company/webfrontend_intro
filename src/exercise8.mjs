5fc4a4f8164dd25c8a3f8e03678075ca65811b16
Takeru.Yamada <takeru.yamada@access-company.com>
2019-05-24 11:55:21 +0900

    Ex.8 Promise

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,11 @@
 import fetch from 'node-fetch';
 
+// 非同期に処理するAPI
 fetch('https://api.github.com/users/github')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson));
+  .then(myjson  => fetch(myjson.followers_url))
+  .then(responce => responce.json())
+  .then(myjson => console.log(myjson))
+
+// こちらが先に出力される
+console.log("hello")
