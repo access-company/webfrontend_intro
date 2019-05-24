a40de4ada4576489030dc26d6b86c74be302e09a
aZhouhai <zhou.hai@access-company.com>
2019-05-24 10:12:36 +0900

    exercise6

---
+++ src/exercise6.mjs
@@ -0,0 +1,7 @@
+import User from './exercise5.mjs';
+const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
+console.log(user1.getName());
+console.log(user1.getAge());
+
+const user2 = new User('74ff0520d615329720235156', 'Hanako', 14);
+console.log(`${user1.name} is ${user2.name}: ${User.equal(user1, user2)}`);
