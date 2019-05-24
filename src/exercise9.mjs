962fefa0c6d93eadf59b4585c0ddf8bbabe64475
Ryo Yamashita <ryo.yamashita@access-company.com>
2019-05-24 10:18:22 +0900

    excercise9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -2,7 +2,9 @@ import fetch from 'node-fetch';
 
 async function fetchFollowers() {
   const url           = 'https://api.github.com/users/diescake';
-  const followers_res = await fetch(url);
+  const response = await fetch(url);
+  const json = await response.json();
+  const followers_res = await fetch(json.followers_url);
   const followers_json = await followers_res.json();
   console.log(followers_json);
 }
