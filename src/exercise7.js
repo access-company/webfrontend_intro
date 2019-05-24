714a39aaaf68f48cce521e447e642fb104107c19
Masamitsu.Yakushiji <Masamitsu.Yakushiji@Y55-66727.local>
2019-05-24 11:19:18 +0900

    ex7

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,27 @@
+
+
 setTimeout(() => {
   console.log('Hello');
+  setTimeout(() => {
+    console.log('World');
+    setTimeout(() => {
+      console.log('!');
+      setTimeout(() => {
+        console.log('!');
+      }, 1000);
+    }, 2000);
+  }, 3000);
 }, 1000);
+
+
+const derayPrint = (str,t,) => {
+  setTimeout(() => {
+   console.log(`${str}`);
+  }, t);
+  return true;
+}
+
+//derayPrint("Hello",100)
+//derayPrint("World",300)
+//derayPrint("!",200)
+//derayPrint("!",100)
