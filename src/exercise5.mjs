50db32c2971e53b956db9011abff74c26bacdeb2
Takeru.Yamada <takeru.yamada@access-company.com>
2019-05-24 10:06:59 +0900

    Ex.6 export & import

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -25,10 +25,13 @@ class User {
     }
   }
 }
-const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
-console.log(user1.getName());
-console.log(user1.getAge());
 
-const user2 = new User('74ff0520d615329720235156', 'Hanako', 14);
-console.log(`${user1.name} is ${user2.name}: ${User.equal(user1, user2)}`);
+export default User;
+
+// const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
+// console.log(user1.getName());
+// console.log(user1.getAge());
+
+// const user2 = new User('74ff0520d615329720235156', 'Hanako', 14);
+// console.log(`${user1.name} is ${user2.name}: ${User.equal(user1, user2)}`);
 
