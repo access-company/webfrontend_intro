a2ef42977e9cabc6ccdb9f244d18cba20009eb47
hiroshi.nishigami@access-company.com <Hiroshi.Nishigami@Y55-66745.local>
2019-05-24 12:57:17 +0900

    [fix] 課題の意味を取り違えていたので、修正。 	modified:   exercise8.mjs 	modified:   exercise9.mjs

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -1,5 +1,18 @@
 import fetch from 'node-fetch';
 
-fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
+fetch('https://api.github.com/users/diescake')
   .then(response => response.json())
-  .then(myJson   => console.log(myJson.followers_url ));
+  .then(myJson   => fetch( myJson.followers_url ))
+  .then(response => response.json())
+  .then(myJson   => console.log(myJson));
+
+//fetch('http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json')
+/*fetch('https://api.github.com/users/diescake')
+  .then(response => response.json())
+  .then(myJson   => fetch( myJson.followers_url )
+    .then(response => response.json())
+    .then(myJson   => console.log(myJson))
+  );
+*/
+
+
--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,10 +1,14 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json';
+  //const url           = 'http://tama.tok.access-company.com/user/soichiro.isobe/training/fetch/users/github.json';
+  const url           = 'https://api.github.com/users/diescake';
   const response = await fetch(url);
   const myJson = await response.json();
-  console.log(myJson.followers_url);
+  const response2 = await fetch(myJson.followers_url);
+  const myJson2 = await response2.json();
+  //const myJson2 = await response2.json();
+  console.log(myJson2);
 }
 
 fetchFollowers();
