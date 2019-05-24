1a7405868b664228357d89314b97be6f319ecfe1
aZhouhai <zhou.hai@access-company.com>
2019-05-24 09:51:57 +0900

    exercise5

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -5,8 +5,17 @@ class User {
     this.id = id;
   }
   // prototypeメソッド getName()を実装する
+  getName(){
+    return this.name;
+  };
   // prototypeメソッド getAge()を実装する
+  getAge(){
+    return this.age
+  };
   // staticメソッド equal(user1, user2)を実装する
+  static equal(user1,user2){
+    return user1.id == user2.id 
+  };
 }
 const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
 console.log(user1.getName());
