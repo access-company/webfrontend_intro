676f93297183cf6e5d26596f595daa7a665ca703
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-24 10:41:59 +0900

    Fixed exercise5.mjs

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -11,12 +11,7 @@ class User {
     return this.age;
   }
   static equal(user1, user2) {
-    if (user1 == user2) {
-      return 'true';
-    }
-    else {
-      return 'false';
-    }
+    return user1.id === user2.id
   }
 }
 
