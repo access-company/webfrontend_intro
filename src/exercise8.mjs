e3775a249d44056347e1cf997573dc105cba2fc2
aTenshouSu <zou.tianxiang@access-company.com>
2019-05-24 11:53:11 +0900

    Fixed exercise8.mjs

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -2,6 +2,6 @@ import fetch from 'node-fetch';
 
 fetch('https://api.github.com/users/diescake')
   .then(response => response.json())
-  .then(json     => fetch(json.followers_url)
+  .then(json     => fetch(json.followers_url))
                     .then(res => res.json())
-                    .then(res => console.log(res)));
+                    .then(res => console.log(res));
