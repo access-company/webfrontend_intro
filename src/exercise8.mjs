ac3aef9052f8dab4e35f11e7d2795169663d03ae
haruka akaba <haruka.akaba@access-company.com>
2019-05-24 11:56:19 +0900

    fix Ex.8

--- src/exercise8.mjs
+++ src/exercise8.mjs
@@ -2,8 +2,6 @@ import fetch from 'node-fetch';
 
 fetch('https://api.github.com/users/diescake')
   .then(response => response.json())
-  .then(myJson   => myJson.followers_url)
-  .then(followers_url => fetch(followers_url)
-    .then(response => response.json())
-    .then(myJson   => console.log(myJson))
-    );
+  .then(myJson   => fetch(myJson.followers_url))
+  .then(response => response.json())
+  .then(myJson   => console.log(myJson));
