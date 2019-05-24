4c52cb5811090fccb2dd9667118f814ff19e249b
Masamitsu.Yakushiji <Masamitsu.Yakushiji@Y55-66727.local>
2019-05-24 10:00:47 +0900

    ex5

--- src/exercise5.mjs
+++ src/exercise5.mjs
@@ -17,6 +17,9 @@ class User {
     return user1.id==user2.id;
   }
 }
+
+export default User;
+
 const user1 = new User('51ff0475d615329700235136', 'Taro', 14);
 console.log(user1.getName());
 console.log(user1.getAge());
