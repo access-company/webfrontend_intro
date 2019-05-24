8b4e7085d1f22532eb65a76cf4dd66b5bd64f616
Takeru.Yamada <takeru.yamada@access-company.com>
2019-05-24 11:16:55 +0900

    Ex.7 setTimeout()

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,13 @@
+
 setTimeout(() => {
   console.log('Hello');
+    setTimeout(() => {
+      console.log('World');
+        setTimeout(() => {
+          console.log("!");
+            setTimeout(() => {
+              console.log("!");
+            }, 1000);
+        }, 2000);
+    }, 3000);
 }, 1000);
