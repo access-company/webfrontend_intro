69e95459e41514bd75638aebfe0c2cfec4642fa2
aZhouhai <zhou.hai@access-company.com>
2019-05-24 10:24:21 +0900

    exercise7

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,13 @@
 setTimeout(() => {
   console.log('Hello');
 }, 1000);
+setTimeout(() => {
+	console.log("world");
+},4000);
+
+suprise = () =>{
+	console.log("!");
+}
+
+setTimeout(suprise,6000);
+setTimeout(suprise,7000);
