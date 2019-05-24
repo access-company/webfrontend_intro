1db254e7f0f5af34b5d1b16d55556864d131505d
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2019-05-24 10:15:27 +0900

    fix ex9

--- src/exercise9.mjs
+++ src/exercise9.mjs
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 async function fetchFollowers() {
-  const url           = 'https://api.github.com/users/github';
+  const url           = 'https://api.github.com/users/diescake';
   const usersResponse = await fetch(url);
   const users         = await usersResponse.json();
   const followers     = await fetch(users.followers_url);
