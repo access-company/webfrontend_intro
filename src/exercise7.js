c327cbccdb5b4ec1cccc57653c4f145f3a01199a
aTakatoNakamura <takato.nakamura@access-company.com>
2019-05-24 11:06:10 +0900

    exercise7

--- src/exercise7.js
+++ src/exercise7.js
@@ -1,3 +1,12 @@
 setTimeout(() => {
   console.log('Hello');
+  setTimeout(() => {
+    console.log('World');
+    setTimeout(() =>{
+      console.log('!');
+      setTimeout(() => {
+        console.log('!');
+      },1000);
+    },2000);
+  },3000);
 }, 1000);
