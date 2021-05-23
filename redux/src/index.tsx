411f09094407d80fe2c6c5dc9eeaaae7bc8a2dd9
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2021-05-23 23:22:12 +0900

    (2) Apply persist

--- redux/src/index.tsx
+++ redux/src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './store'
+import { store, persistor } from './store'
 import TodoApp from './pages/TodoApp'
 
 ReactDOM.render(
   <Provider store={store}>
-    <TodoApp />
+    <PersistGate loading={null} persistor={persistor}>
+      <TodoApp />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
--- redux/src/store.ts
+++ redux/src/store.ts
@@ -1,12 +1,21 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export const persistor = persistStore(store as any)
 
 console.log(store.getState())
 
 export type RootState = ReturnType<typeof store.getState>
-
-export default store
