2e6a5f3db3883dc369d0b3b1c45984b4431bccb9
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2022-05-17 21:00:00 +0900

    [#26] Downgrade React

--- redux/src/index.tsx
+++ redux/src/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
