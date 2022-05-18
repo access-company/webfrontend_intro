e535689f09a0dbee9d57a8d78641c58772d9b5e0
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2022-05-18 19:41:07 +0900

    [#26] Remove API logic from BookCreateForm

--- redux/src/components/BookCreateForm/index.tsx
+++ redux/src/components/BookCreateForm/index.tsx
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { fetchAuthorsAsync, selectAuthors, selectAuthorsStatus } from '../../slices/authors';
-import {
-  createBookAsync,
-} from '../../slices/books';
+import { Author } from '../../models/Author';
 import { Presenter } from './Presenter';
 
-export function BookCreateForm() {
-  const authors = useAppSelector(selectAuthors);
-  const authorsStatus = useAppSelector(selectAuthorsStatus);
-  const dispatch = useAppDispatch();
+interface Props {
+  states: {
+    authors: Author[]
+  }
+  actions: {
+    createBook: (title: string, authorId: number) => void
+  }
+}
+
+export function BookCreateForm(props: Props) {
+  const {
+    states: { authors },
+    actions: { createBook },
+  } = props
 
   const [title, setTitle] = useState('')
   const [authorId, setAuthorId] = useState<number>(authors[0]?.id)
-
-  useEffect(() => {
-    if (authorsStatus === 'initial') {
-      dispatch(fetchAuthorsAsync())
-    }
-  }, [dispatch, authorsStatus])
 
   useEffect(() => {
     if (authorId == null) {
       setAuthorId(authors[0]?.id)
     }
   }, [authorId, authors])
-
-  const createBook = (title: string, authorId: number) => { dispatch(createBookAsync({ title, authorId })) }
 
   return (<Presenter
     {
