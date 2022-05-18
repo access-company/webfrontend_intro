3ddea0418c38846a19b6c4aa3a74c1be0bf1a3f9
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2022-05-18 19:40:10 +0900

    [#26] Remove API logic from AuthorsTable

--- redux/src/components/AuthorsTable/index.tsx
+++ redux/src/components/AuthorsTable/index.tsx
@@ -1,31 +1,38 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
-import {
-  fetchAuthorsAsync,
-  selectAuthors,
-  selectAuthorsStatus,
-} from '../../slices/authors';
-import { Presenter } from './Presenter';
+import { Author } from '../../models/Author';
+import styles from './style.module.css';
 
-export function AuthorsTable() {
-  const authors = useAppSelector(selectAuthors);
-  const authorsStatus = useAppSelector(selectAuthorsStatus);
-  const dispatch = useAppDispatch();
+interface Props {
+  states: {
+    authors: Author[]
+  }
+}
 
-  useEffect(() => {
-    if (authorsStatus === 'initial') {
-      dispatch(fetchAuthorsAsync())
-    }
-  }, [dispatch, authorsStatus])
-
-  return (<Presenter
-    {
-      ...{
-        states: {
-          authors,
-        }
-      }
-    }
-  />)
+export function AuthorsTable(props: Props) {
+  const {
+    states: {
+      authors,
+    },
+  } = props
+  return (
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr>
+          <th>Id</th>
+          <th>First name</th>
+          <th>Last name</th>
+        </tr>
+      </thead>
+      <tbody className={styles.tbody}>
+        {authors.map(({ id, firstName, lastName }) => (
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{firstName}</td>
+            <td>{lastName}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
