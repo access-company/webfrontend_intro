014da991faf9d48a783279c7a956c83685b4e3c1
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2022-05-23 04:51:36 +0900

    [#26] Remove unused import

--- redux/src/slices/books/index.ts
+++ redux/src/slices/books/index.ts
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { WritableDraft } from 'immer/dist/internal';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import { fetchBooks, createBook } from '../../apis/books';
 import { RootState } from '../../app/store';
