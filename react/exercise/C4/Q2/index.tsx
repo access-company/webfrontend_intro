60f6591945e2b94eebe76b2910725eba9828c1ea
Kensuke Takahara <kensuke.takahara@access-company.com>
2023-05-08 11:06:58 +0900

    Implemented Chapter4

---
+++ react/exercise/C4/Q2/index.tsx
@@ -0,0 +1,41 @@
+// C4/Q2
+// 以下の要件を満たしてください
+// * 画面上の 2 つの `<div>` にそれぞれ下記を満たす click イベントのハンドラを追加する
+//   * 赤い `<div>` をクリックした際には `Clicked Red` のみコンソールに表示する
+//   * 青い `<div>` をクリックした際には `Clicked Blue` のみコンソールに表示する
+//
+import { FC } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+
+// ’Clicked Blue’ とコンソールに表示するイベントハンドラー 'onClickInner' を実装
+
+// 'Clicked Red' とコンソールに表示するイベントハンドラー 'onClickOuter' を実装
+
+// 赤い部分を押したら 'Clicked Red', 青い部分を押したら 'Clicked Blue' のみ表示されるようにする
+
+const onClickInner: FC = (e: React.MouseEvent<HTMLDivElement>) => {};
+
+const onClickOuter: FC = (e: React.MouseEvent<HTMLDivElement>) => {};
+
+const ChildComponent: FC = () => {
+  return (
+    <div className="subContainer">
+      <div className="outer" onClick={}>
+        <div className="inner" onClick={}>
+          Matryoshka
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const App: FC = () => {
+  return (
+    <div>
+      <ChildComponent />
+    </div>
+  );
+};
+
+createRoot(document.querySelector(".container")!).render(<App />);
