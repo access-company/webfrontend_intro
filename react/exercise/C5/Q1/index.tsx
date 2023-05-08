4a2e1c3ae8df9017215b3b25fd8800cc4b556311
Kensuke Takahara <kensuke.takahara@access-company.com>
2023-05-08 16:38:42 +0900

    add answer5.

--- react/exercise/C5/Q1/index.tsx
+++ react/exercise/C5/Q1/index.tsx
@@ -34,7 +34,7 @@ const isValidValue = (value: string): boolean =>
 
 const ListItem: FC<ListItemProps> = (props) => {
   const { value } = props;
-  return <div className="normal">{value}</div>;
+  return <div className={isValidValue(value) ? "red" : "blue"}>{value}</div>;
 };
 
 const ChildComponent: FC = () => {
