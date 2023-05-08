f41f19a879f80ee858539171e1bc98d06a596b22
Kensuke Takahara <kensuke.takahara@access-company.com>
2023-05-08 12:01:59 +0900

    fix chapter8

--- react/exercise/C8/Q3/index.tsx
+++ react/exercise/C8/Q3/index.tsx
@@ -32,13 +32,12 @@ const LightBulb: FC<LightBulbProps> = (props) => {
           height="44"
           viewBox="0 0 24 24"
           stroke-width="1.5"
-          stroke="#2c3e50"
+          stroke="#ffffff"
           fill="none"
           stroke-linecap="round"
           stroke-linejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-          <path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7" />
           <path d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3" />
           <line x1="9.7" y1="17" x2="14.3" y2="17" />
         </svg>
@@ -49,12 +48,13 @@ const LightBulb: FC<LightBulbProps> = (props) => {
           height="44"
           viewBox="0 0 24 24"
           stroke-width="1.5"
-          stroke="#ffffff"
+          stroke="#2c3e50"
           fill="none"
           stroke-linecap="round"
           stroke-linejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7" />
           <path d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3" />
           <line x1="9.7" y1="17" x2="14.3" y2="17" />
         </svg>
@@ -73,7 +73,7 @@ const Switch: FC<SwitchProps> = (props) => {
         height="44"
         viewBox="0 0 24 24"
         stroke-width="1.5"
-        stroke={power ? "#2c3e50" : "#ffffff"}
+        stroke={power ? "#ffffff" : "#2c3e50"}
         fill="none"
         stroke-linecap="round"
         stroke-linejoin="round"
