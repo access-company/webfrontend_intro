f1cf2edc72d374976537d85382ea15baf2f05560
Nobuhisa Masaki <nobuhisa.masaki@access-company.com>
2023-05-09 20:07:03 +0900

    Fix React C8 Q3 CSS (#54)

--- react/exercise/C08/Q3/index.tsx
+++ react/exercise/C08/Q3/index.tsx
@@ -31,11 +31,11 @@ const LightBulb: FC<LightBulbProps> = (props) => {
           width="44"
           height="44"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="#2c3e50"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3" />
@@ -47,11 +47,11 @@ const LightBulb: FC<LightBulbProps> = (props) => {
           width="44"
           height="44"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="#ffffff"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7" />
@@ -72,11 +72,11 @@ const Switch: FC<SwitchProps> = (props) => {
         width="44"
         height="44"
         viewBox="0 0 24 24"
-        stroke-width="1.5"
+        strokeWidth="1.5"
         stroke={power ? "#2c3e50" : "#ffffff"}
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
         <path d="M7 6a7.75 7.75 0 1 0 10 0" />
