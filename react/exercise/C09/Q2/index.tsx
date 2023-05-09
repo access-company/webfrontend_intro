be73d8252033540a7faf16b7f4982c8edc7f05d6
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2023-05-09 18:57:00 +0900

    Forgot to import CSS

--- react/exercise/C09/Q2/index.tsx
+++ react/exercise/C09/Q2/index.tsx
@@ -14,6 +14,7 @@
 //
 import { FC, useReducer, useState } from "react";
 import { createRoot } from "react-dom/client";
+import "./style.css";
 
 declare type CarIndicatorProps = {
 	fulfilled: boolean;
