3e7ae1d18e45f2f10aa41e87c9b9a670610fa56c
ka-clmx <masashi.takano@access-company.com>
2024-05-08 17:22:15 +0900

    Replace Sample App enviornment

--- react/sample/src/App.tsx
+++ react/sample/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './App.module.css'
 
-import Calculator from './containers/Calculator'
+import Calculator from './view/containers/Calculator'
 
 function App(): JSX.Element {
   return (
---
+++ react/sample/src/view/containers/Calculator/Calculator.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Calculator from './Calculator';
+
+export default {
+  title: 'containers/Calculator',
+  component: Calculator,
+  args: {},
+} as Meta;
+
+export const Deault: StoryObj<typeof Calculator> = {};
