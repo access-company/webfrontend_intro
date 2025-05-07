f100259f07d97b3a3de470be087402f4a9a0ee82
Anna.Chiba <anna.chiba@access-company.com>
2025-05-07 21:31:07 +0900

    fix vite config

--- typescript/src/chrome_extention/vite.config.js
+++ typescript/src/chrome_extention/vite.config.js
@@ -1,19 +1,17 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
-export default defineConfig((opt) => {
-  return {
-    root: "src",
-    build: {
-      outDir: "../dist",
-      rollupOptions: {
-        input: {
-          index: resolve(__dirname, "src/index.ts"),
-        },
-        output: {
-          entryFileNames: "[name].js",
-        },
+export default defineConfig(() => ({
+  root: "./src",
+  build: {
+    outDir: "./dist",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
       },
     },
-  };
-});
+  },
+}));
